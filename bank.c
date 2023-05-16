04b7fc6239714deded13cb6e83b13ebceaa4a359
ilanhalioua <113393627+ilanhalioua@users.noreply.github.com>
2023-05-16 15:41:14 +0200

    Update bank.c

--- bank.c
+++ bank.c
@@ -12,28 +12,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+//global variables and constants:
+#define MAX_OPS 200
+
 int num_ATMs;
 int num_workers;
 
-#define MAX_LINE_LENGTH 100 ///
 int max_operations;
 
 int end = 0;
-struct queue *circular_queue;
-struct element *list_client_ops;
+struct queue *circular_queue; //circular queue (buffer) shared by producers and consumer threads
+struct element *list_client_ops; //dynamic array of operations 
 
 int client_numop = 0; //counter of productions
 int bank_numop = 0; //counter of consumptions
 
 int started = 0; //0 -> 'false', 1 -> 'true'
-pthread_mutex_t mutex;
-pthread_cond_t start;
+pthread_mutex_t mutex; //mutex lock to synchronize threads
+pthread_cond_t start; 
 
-pthread_cond_t notFull; 
-pthread_cond_t notEmpty;
+pthread_cond_t notFull; //condition variable on the queue being not full 
+pthread_cond_t notEmpty; //condition variable on the queue being not empty 
 
-float global_balance = 0.0;
-float *account_balance ;
+float global_balance = 0.0; //total balance of the bank
+float *account_balance ; //dynamic array of integers (of size = <max_account>) that stores the ith account current balance at ith index
 
 
 void *ATM(void *param) { // Producer thread
@@ -41,7 +43,7 @@ void *ATM(void *param) { // Producer thread
 	
 	int id;
 	struct element elem;
-	int amount_to_produce = *((int*)param);
+	int amount_to_produce = *((int*)param); //typecast the void pointer params and dereference it in order to obtain the actual number of operations to process (the one in the first line of the file)
 	
 	// Producer thread has started:
 	if (pthread_mutex_lock(&mutex) < 0){
@@ -59,8 +61,6 @@ void *ATM(void *param) { // Producer thread
 	//Producing
 	while(client_numop < amount_to_produce) //producer(atm) must work while there are operations to produce
 	{	
-	
-		/////printf("PRODUCER: %d, Petition_value: %d, ElementId: %d, Amount: %f\n", id, client_numop, elem.operation_id, elem.amount);
 		
 		// Critical section beggin:
 		if (pthread_mutex_lock(&mutex) < 0){
@@ -68,23 +68,23 @@ void *ATM(void *param) { // Producer thread
     			exit(-1);
   		}
 		while (queue_full(circular_queue)){ // while queue is full
-			if (pthread_cond_wait(&notFull, &mutex) < 0){
+			if (pthread_cond_wait(&notFull, &mutex) < 0){ //waits until signal indicating that queue is not full (releases the mutex)
 				perror("Condition variable error");
     				exit(-1);
 			}
 		}
 		
-		elem = list_client_ops[client_numop];
-		client_numop += 1;
+		elem = list_client_ops[client_numop]; 
+		client_numop += 1; //increment by 1 the number of productions done
 		
 		// Insert in circular buffer the produced element
-		if(queue_put(circular_queue, &elem) < 0){
+		if(queue_put(circular_queue, &elem) < 0){ //enqueue the operation from the array at index client_numop
 		      perror("Failed to enqueue an element");
 		      exit(-1);
 		}
 		    
 		// New element produced warn and critical section end:
-		if(pthread_cond_signal(&notEmpty) < 0){
+		if(pthread_cond_signal(&notEmpty) < 0){ 
 		      perror("Error while warning that queue is not empty");
 		      exit(-1);
 		}
@@ -96,7 +96,6 @@ void *ATM(void *param) { // Producer thread
 	
 	
 	// End producer execution
-	/////printf("Producer: %d. End\n", id);
 	pthread_exit(0);
 	return NULL;
 
@@ -107,7 +106,7 @@ void *Worker(void *param) { // Consumer thread
 	
 	int id;
 	struct element* data;
-	int amount_to_consume = *((int*)param);
+	int amount_to_consume = *((int*)param); //typecast the void pointer params as an int pointer and dereference it to access the number of ops to process (the one in the first line of the input file)
 	
 	// Consumer thread has started:
 	pthread_mutex_lock(&mutex);
@@ -128,7 +127,6 @@ void *Worker(void *param) { // Consumer thread
 		while (queue_empty(circular_queue)){ // while queue is empty
 			if (end == 1) // Queue is empty and we have finished
 			{
-				/////printf("Consumer: %d. End\n", id);
 				if (pthread_mutex_unlock(&mutex) < 0){
     					perror("Mutex error");
     					exit(-1);
@@ -144,8 +142,9 @@ void *Worker(void *param) { // Consumer thread
 		}
 		
 		// Remove consumed element from circular buffer
-		data = queue_get(circular_queue);	
+		data = queue_get(circular_queue); //dequeue the operation on the circular queue	
 		
+		//take the respective action and print info on screen
 		switch (data->operation_id) {
 		    case 1: // CREATE
 		      account_balance[data->account_number] = 0;
@@ -175,8 +174,8 @@ void *Worker(void *param) { // Consumer thread
 		      perror("Not valid operation");
 		} 
 		
-		/////printf("CONSUMER: %d, Petition_value: %d, ElementId: %d\n", id, bank_numop, data->operation_id);
-		bank_numop +=1;
+		bank_numop +=1; //increment by 1 the number of consumptions done
+		
 		// Element consumed warn and critical section end:
 		if (pthread_cond_signal(&notFull) < 0){
     			perror("Error while warning that queue is not full");
@@ -190,7 +189,6 @@ void *Worker(void *param) { // Consumer thread
 	}
 	
 	// End producer execution
-	/////printf("Consumer: %d. End\n", id);
 	pthread_exit(0);
 	return (void*)(long)1;
 }
@@ -222,9 +220,11 @@ int fileLines(const char filename[]) {
 }
  
 int main (int argc, const char * argv[] ) {
-
-	// ./bank <file name> <num ATMs> <num workers> <max accounts> <buff size>	
+	/* NOTE: The program is executed on the following way:
+	    ./bank <file name> <num_ATMs> <num_workers> <max_accounts> <buff_size>
+	*/	
 	
+	//store the input data inside variables for readability
 	num_ATMs = atoi(argv[2]);
 	num_workers = atoi(argv[3]);
 	int max_accounts = atoi(argv[4]);
@@ -246,7 +246,7 @@ int main (int argc, const char * argv[] ) {
 	
 
 	
-	// 1) READ FROM FILE
+	// 1) READ FROM FILE (and store operations inside the array of type elements: list_client_ops[])
 	
 	FILE *fp = fopen(argv[1], "r");
 	    if (fp == NULL) {
@@ -259,27 +259,25 @@ int main (int argc, const char * argv[] ) {
     		perror("Error retrieving data from input file");
     		exit(-1);
   	    }
-	    /////printf("Number of Operations to be processed: %d\n\n", max_operations);
 	    
 	    list_client_ops = (struct element*)malloc(max_operations * sizeof(struct element)); //array of elements to be inserted by main process from the text file
 	    
-	    if (max_operations > 200) {
+	    if (max_operations > MAX_OPS) {
 	    	perror("Maximum number of operations is: 200");
     		return -1;
 	    }
 	    
 	    nlines = fileLines(argv[1]);
-	    /////printf("max_operations:%d vs nlines - 1: %d\n", max_operations, nlines-1);
 	    if (nlines - 1 != max_operations) 
 	    {
 	        perror("Number of operations in file should match with the value in the first line");
     		return -1;
 	    } 
 
-	    char line[MAX_LINE_LENGTH];
+	    char line[100];
 	    int i = 0;
 	    
-	    while (fgets(line, MAX_LINE_LENGTH, fp) != NULL) {
+	    while (fgets(line, 100, fp) != NULL) {
 	    	// Initialize amount to 0 for each new element
         	list_client_ops[i].amount = 0.0;
 	    
@@ -288,14 +286,12 @@ int main (int argc, const char * argv[] ) {
 		    token = strtok(NULL, " ");
 		    list_client_ops[i].operation_id = 1;
 		    list_client_ops[i].account_number = atoi(token);
-		    /////printf("Element: list_client_ops[%d] -> opId = %d (CREATE), accNum = %d\n",i, list_client_ops[i].operation_id, list_client_ops[i].account_number);
 		} else if (strcmp(token, "DEPOSIT") == 0) {
 		    token = strtok(NULL, " ");
 		    list_client_ops[i].operation_id = 2;
 		    list_client_ops[i].account_number = atoi(token);
 		    token = strtok(NULL, " ");
 		    list_client_ops[i].amount = atof(token);
-		    /////printf("Element: list_client_ops[%d] -> opId = %d (DEPOSIT), accNum = %d, amount = %.2f\n",i, list_client_ops[i].operation_id, list_client_ops[i].account_number, list_client_ops[i].amount);
 		} else if (strcmp(token, "TRANSFER") == 0) {
 		    token = strtok(NULL, " ");
 		    list_client_ops[i].operation_id = 3;
@@ -304,19 +300,16 @@ int main (int argc, const char * argv[] ) {
 		    list_client_ops[i].acc_to = atoi(token);
 		    token = strtok(NULL, " ");
 		    list_client_ops[i].amount = atof(token);
-		    /////printf("Element: list_client_ops[%d] -> opId = %d (TRANSFER), acc_from = %d, acc_to = %d, amount = %.2f\n",i, list_client_ops[i].operation_id, list_client_ops[i].acc_from, list_client_ops[i].acc_to, list_client_ops[i].amount);
 		} else if (strcmp(token, "WITHDRAW") == 0) {
 		    token = strtok(NULL, " ");
 		    list_client_ops[i].operation_id = 4;
 		    list_client_ops[i].account_number = atoi(token);
 		    token = strtok(NULL, " ");
 		    list_client_ops[i].amount = atof(token);
-		    /////printf("Element: list_client_ops[%d] -> opId = %d (WITHDRAW), accNum = %d, amount = %.2f\n",i, list_client_ops[i].operation_id, list_client_ops[i].account_number, list_client_ops[i].amount);
 		} else if (strcmp(token, "BALANCE") == 0) {
 		    token = strtok(NULL, " ");
 		    list_client_ops[i].operation_id = 5;
 		    list_client_ops[i].account_number = atoi(token);
-		    /////printf("Element: list_client_ops[%d] -> opId = %d (BALANCE), accNum = %d\n",i, list_client_ops[i].operation_id, list_client_ops[i].account_number);
 		}
 		i++;
 	    }
@@ -324,12 +317,6 @@ int main (int argc, const char * argv[] ) {
 	    fclose(fp);
 	    
 	// FINISH READING
-	
-	// ------ delete
-	/////printf("\n\n");
-	/////printf("ElementId Leyend:\n1 -> 'CREATE', 2 -> 'DEPOSIT', 3 -> 'TRANSFER', 4 -> 'WITHDRAW', 5 -> 'BALANCE'\n");
-	/////printf("\n");
-	// ------ delete
 	
 	void *retval;
 	pthread_t threads[num_ATMs + num_workers];
